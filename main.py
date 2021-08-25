50e9eedb454aef692d55ea4dd83d8f935a7e3c35
RaulOrban <raulorban1@gmail.com>
2021-08-25 16:53:49 -0400

    Add bootstrap and home,profile links

---
+++ main.py
@@ -0,0 +1,21 @@
+import werkzeug.security
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__, static_folder='static')
+Bootstrap(app)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
